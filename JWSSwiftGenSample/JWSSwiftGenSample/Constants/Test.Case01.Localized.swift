34a402f67e51211466e72d8096e2227baa973dcd
Clint Jang <jang.wangsu@gmail.com>
2018-02-27 19:22:50 +0900

    add test project files

---
+++ JWSSwiftGenSample/JWSSwiftGenSample/Constants/Test.Case01.Localized.swift
@@ -0,0 +1,28 @@
+// Generated using SwiftGen, by O.Halligon — https://github.com/SwiftGen/SwiftGen
+
+import Foundation
+
+// swiftlint:disable superfluous_disable_command
+// swiftlint:disable file_length
+
+// swiftlint:disable explicit_type_interface identifier_name line_length nesting type_body_length type_name
+enum TestStrings {
+  /// test string 1
+  static let testCase01SubIndex01 = TestStrings.tr("TestCase01Localizable", "test_case01_sub_index01")
+  /// test string 2
+  static let testCase01SubIndex02 = TestStrings.tr("TestCase01Localizable", "test_case01_sub_index02")
+  /// title sample 2
+  static let testCase01TitleBottom01 = TestStrings.tr("TestCase01Localizable", "test_case01_title_bottom01")
+  /// title sample 1
+  static let testCase01TitleTop01 = TestStrings.tr("TestCase01Localizable", "test_case01_title_top01")
+}
+// swiftlint:enable explicit_type_interface identifier_name line_length nesting type_body_length type_name
+
+extension TestStrings {
+  fileprivate static func tr(_ table: String, _ key: String, _ args: CVarArg...) -> String {
+    let format = NSLocalizedString(key, tableName: table, bundle: Bundle(for: BundleToken.self), comment: "")
+    return String(format: format, locale: Locale.current, arguments: args)
+  }
+}
+
+private final class BundleToken {}
---
+++ JWSSwiftGenSample/JWSSwiftGenSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JWSSwiftGenSample
+//
+//  Created by Jang Wangsu on 2018. 2. 23..
+//  Copyright © 2018년 clint. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
